b85053ea8515a926e58b6ec8b6ee8d2200758af6
WOLFIE_OG <oliverjtodd@yahoo.com>
2023-11-29 18:17:01 +0000

    Stuff

---
+++ libft/ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 13:21:14 by otodd             #+#    #+#             */
+/*   Updated: 2023/11/17 12:12:41 by otodd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*proxy_d;
+	unsigned const char	*proxy_s = src;
+	unsigned char		*end_d;
+	unsigned char		*end_s;
+
+	proxy_d = dest;
+	if (proxy_d < proxy_s)
+		while (n--)
+			*proxy_d++ = *proxy_s++;
+	else
+	{
+		end_d = proxy_d + (n - 1);
+		end_s = (unsigned char *)proxy_s + (n - 1);
+		while (n--)
+			*end_d-- = *end_s--;
+	}
+	return (dest);
+}
---
+++ libft/ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 17:20:33 by otodd             #+#    #+#             */
+/*   Updated: 2023/11/17 12:13:17 by otodd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	d_len;
+	size_t	s_len;
+	size_t	index;
+	size_t	result;
+
+	d_len = ft_strlen(dst);
+	s_len = ft_strlen((char *)src);
+	index = 0;
+	result = 0;
+	if (size <= d_len)
+		result = s_len + size;
+	else
+	{
+		result = s_len + d_len;
+		while (src[index] && d_len < size - 1)
+			dst[d_len++] = src[index++];
+	}
+	dst[d_len] = '\0';
+	return (result);
+}
